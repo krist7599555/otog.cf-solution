3aa4512d716e6c61b6e7c1cacadf6c6483400d15
Krist Ponpairin <krist7599555@gmail.com>
2023-02-09 23:39:29 +0700

    feat: 5 beter variable name

--- 5.cpp
+++ 5.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <cassert>
 
-enum Scales { left, right, skip };
+enum Scales { lighter, heavier, skip };
 
 std::vector<Scales> scales_of(int weight) {
   std::vector<Scales> res; // res[i] is in multiply of pow(3, i)
@@ -14,11 +14,11 @@ std::vector<Scales> scales_of(int weight) {
       res.push_back(Scales::skip);
       break;
     case 1:
-      res.push_back(Scales::right);
+      res.push_back(Scales::heavier);
       weight -= 1;
       break;
     case 2:
-      res.push_back(Scales::left);
+      res.push_back(Scales::lighter);
       weight += 1;
       break;
     }
@@ -27,26 +27,26 @@ std::vector<Scales> scales_of(int weight) {
 }
 
 int main() {
-  int scales_left;
-  std::cin >> scales_left;
+  int total_weight_left;
+  std::cin >> total_weight_left;
 
-  int scales_right = 0;
-  int count_weighter = 0;
+  int total_weight_right = 0;
+  int count_mass_use = 0;
   int pow3 = 1;
-  for (Scales fac : scales_of(scales_left)) {
-    switch (fac) {
-    case Scales::left:
-      scales_left += pow3;
-      count_weighter += 1;
+  for (Scales mass : scales_of(total_weight_left)) {
+    switch (mass) {
+    case Scales::lighter:
+      total_weight_left += pow3;
+      count_mass_use += 1;
       break;
-    case Scales::right:
-      scales_right += pow3;
-      count_weighter += 1;
+    case Scales::heavier:
+      total_weight_right += pow3;
+      count_mass_use += 1;
       break;
     case Scales::skip:;
     }
     pow3 *= 3;
   }
-  assert(scales_right == scales_left);
-  std::cout << count_weighter << ' ' << scales_left << std::endl;
+  assert(total_weight_right == total_weight_left);
+  std::cout << count_mass_use << ' ' << total_weight_left << std::endl;
 }
